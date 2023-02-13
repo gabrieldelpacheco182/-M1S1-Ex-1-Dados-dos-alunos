35e50ac3447f767ec377ba4e974704a2c9665cca
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-02-13 14:41:03 -0300

    exercicio 1

---
+++ functino.js
@@ -0,0 +1,38 @@
+let students = [
+    {
+        name: 'Gabriel',
+        height: 1.77,
+        age: 36,
+        sex: 'Masculino',
+        room: "Trindade",
+    },
+    {
+        name: 'Adriana',
+        height: 1.657,
+        age: 426,
+        sex: 'Feminino',
+        room: "Coaria",
+    },
+    {
+        name: 'Paula',
+        height: 1.70,
+        age: 25,
+        sex: 'Feminino',
+        room: "Palhoça",
+    },
+    {
+        name: 'Leonardo',
+        height: 1.83,
+        age: 21,
+        sex: 'Masculino',
+        room: "Palhoça", 
+    },
+    {
+        name: 'Gabriel',
+        height: 1.63,
+        age: 70,
+        sex: 'Masculino',
+        room: "Laguna",
+    },
+];
+console.log(students);
